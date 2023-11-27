19c406acf80e669458f1a3eb53319467aaf05ad5
jason-muncy <144398409+jason-muncy@users.noreply.github.com>
2023-11-27 12:31:57 -0500

    Update cipher.py
    made by hand. shortened code down

--- cipher.py
+++ cipher.py
@@ -1,23 +1,18 @@
-alphabet= {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
-'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
-'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
-'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
-'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
-'z': 26
-}
-sentence= input('Insert message to encrypt: ')
-shift= int(input('What number do you want to shift?'))
+shift = int(input('Please enter a number from 1-26 to shift: '))
+encrypt = str(input('Please enter text to encrypt: '))
+encrypted_message = ""
 
-encrypted_sentence = ''
-for char in sentence:
-    if char.isalpha():
-        is_upper = char.isupper()
-        char = char.lower()  
-        shifted_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
-        if is_upper:
-            shifted_char = shifted_char.upper()
-        encrypted_sentence += shifted_char
-    else:
-        encrypted_sentence += char
+# creating list for a-z
+alphabet = []
 
-print('Encrypted message:', encrypted_sentence)    
+for i in range(ord('a'), ord('z')+1):
+    alphabet.append(chr(i))
+
+for i in encrypt:
+    if i in alphabet:
+        f = alphabet.index(i) + shift
+        if f > 26:
+            f = f % 26
+        encrypted_message += alphabet[f]
+
+print(encrypted_message)  
